d1f8c2322ff555f3d4ea45d77911b6e89dab6afe
NinaRao <ninarao42@gmail.com>
2016-12-19 00:03:32 -0500

    panther input

--- Week11_diffExp/02-diffExp2.py
+++ Week11_diffExp/02-diffExp2.py
@@ -44,13 +44,35 @@ Z = linkage(m, 'average') #calculate linkage distances
 #4) Cluster by k-means 
 x = np.array(m[:, 0]) #cfc
 y = np.array(m[:, 1]) #poly
+genes = []
+f = open(sys.argv[1])
+for line in f.readlines()[1:]:
+	name = line.rstrip("\n").split()[0]
+	genes.append(name)
+#5) plot kmeans2
+#centroid, label = kmeans2(m, 3)
 
-#kmeans2
-centroid, label = kmeans2(m, 2)
-color1 = np.array(["red", "black"])
-plt.scatter(x, y, c=color1[label])
-plt.ylim(-0.15,20) 
-plt.xlim(-0.05,16) 
-plt.savefig("02-kmeans_k2.png")
-plt.show()
+#color1 = np.array(["red", "black"])
+#plt.scatter(x, y, c=color1[label])
+#plt.ylim(-0.15,20) 
+#plt.xlim(-0.05,16) 
+#plt.savefig("02-kmeans_k2.png")
+#plt.show()
+
+#6) find genes that are similar to most up gene, Creb3l3
+centroid, label = kmeans2(m, 5)
+print label[289]
+
+##index of creb is 290. this is group "1"
+creblike = []
+
+for k, g in zip(label,genes):
+	if k==1:
+		creblike.append(g)
+#print len(creblike)		
+f = open("04-creblikeGenes.txt", "w")
+for i in creblike:
+	f.write(i)
+	f.write("\n")
+
 
